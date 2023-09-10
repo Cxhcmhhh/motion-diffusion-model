e43b3629129a8d4d10dc1f1f2a78a4153f6a7d7d
Cxhcmhhh <90499297+Cxhcmhhh@users.noreply.github.com>
2023-09-10 09:35:00 +0800

    Update model_motion_loaders.py

--- data_loaders/humanml/motion_loaders/model_motion_loaders.py
+++ data_loaders/humanml/motion_loaders/model_motion_loaders.py
@@ -89,3 +89,18 @@ def get_mdm_loader(model, diffusion, batch_size, ground_truth_loader, mm_num_sam
     print('Generated Dataset Loading Completed!!!')
 
     return motion_loader, mm_motion_loader
+
+def my_loader(model, diffusion, batch_size, ground_truth_loader, mm_num_samples, mm_num_repeats, max_motion_length, num_samples_limit, scale):
+    opt = {
+        'name': 'test',  # FIXME
+    }
+    print('Generating %s ...' % opt['name'])
+    # dataset = CompMDMGeneratedDataset(opt, ground_truth_dataset, ground_truth_dataset.w_vectorizer, mm_num_samples, mm_num_repeats)
+    dataset = CompMDMGeneratedDataset(model, diffusion, ground_truth_loader, mm_num_samples, mm_num_repeats, max_motion_length, num_samples_limit, scale)
+
+    # NOTE: bs must not be changed! this will cause a bug in R precision calc!
+    motion_loader = DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn, drop_last=True, num_workers=4)
+
+    print('Generated Dataset Loading Completed!!!')
+
+    return motion_loader, mm_motion_loader
